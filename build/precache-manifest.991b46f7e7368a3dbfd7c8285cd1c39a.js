af005b23db374c1b6155716b0aa3a927a2515a4e
iamabby <1127153857@qq.com>
2018-12-21 16:28:16 +0800

    build

---
+++ build/precache-manifest.991b46f7e7368a3dbfd7c8285cd1c39a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6806befc0206320c811ed30354f2b37b",
+    "url": "./static/media/icon-movie.6806befc.png"
+  },
+  {
+    "revision": "9465aff01dbf3e81c62dd69117c4a47c",
+    "url": "./static/media/icon-movie-selected.9465aff0.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "db3b2b54f88f5045b094",
+    "url": "./static/js/main.db3b2b54.chunk.js"
+  },
+  {
+    "revision": "fb3df22a7dd08d0c3c88",
+    "url": "./static/js/1.fb3df22a.chunk.js"
+  },
+  {
+    "revision": "db3b2b54f88f5045b094",
+    "url": "./static/css/main.9bb24b27.chunk.css"
+  },
+  {
+    "revision": "0090e7e3c957ff519332b690af7cd477",
+    "url": "./index.html"
+  }
+];
